ea6f553f270812b9e242549e0a880fd2d980b4d5
burgokmen <burgokmen@gmail.com>
2023-10-26 19:29:27 +0300

    init

---
+++ src/main/java/com/brutech/java_hibernate_advance_mappings/JavaHibernateAdvanceMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.brutech.java_hibernate_advance_mappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHibernateAdvanceMappingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHibernateAdvanceMappingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brutech/java_hibernate_advance_mappings/JavaHibernateAdvanceMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brutech.java_hibernate_advance_mappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHibernateAdvanceMappingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
